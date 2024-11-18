08be16c6744208de897952cabac2cace270082f1
Nursultan <superrandommmm@gmail.com>
2024-11-18 09:29:16 +0300

    Add project files.

---
+++ ThirdHoomework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+
+namespace Tumakov
+{
+    enum Check
+    {
+        Friday, Sunday = 50, Tuesday, Wednesday = 67, Thursday
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            Check work = Check.Friday;
+            work = (Check)1;
+
+
+            Console.WriteLine("Добрый День!");
+            Console.WriteLine((int)Check.Thursday);
+        }
+    }
+}
---
+++ Tumakov/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Text;
+
+
+namespace ThirdHoomework
+{
+    enum Langauge
+    {  
+        cPlus, cSharp, c, python     
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            string LanguageExtender(Langauge langauge)
+            {
+                switch (langauge)
+                {
+                    case Langauge.cPlus:
+                        return "C++";
+                    case Langauge.cSharp:
+                        return "C#";
+                    case Langauge.c:
+                        return "C";
+                    case Langauge.python:
+                        return "Python";
+                    default:
+                        return "Кумир";
+                }
+            }
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            var test1 = Langauge.cSharp;
+            Console.WriteLine("Я люблю {0}", LanguageExtender(test1));
+        }
+    }
+}
